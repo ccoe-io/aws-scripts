b3e187089c58f458ce2cae92cc40510a49a2e418
Ronen Dancziger <ronend4@gmail.com>
2024-02-12 13:37:10 -0500

    refactor

--- actions/iam_role.py
+++ actions/iam_role.py
@@ -8,3 +8,19 @@ def delete_iam_role(session, name):
     
     # Return the response
     return response
+
+def iam_role_exists(session, name):
+    # Create an IAM client
+    client = session.client('iam')
+    
+    # List all IAM roles using pagination
+    paginator = client.get_paginator('list_roles')
+    response_iterator = paginator.paginate()
+    
+    # Check if the role exists
+    for response in response_iterator:
+        for role in response['Roles']:
+            if role['RoleName'] == name:
+                return True
+    
+    return False
--- orchesrator.py
+++ orchesrator.py
@@ -1,7 +1,7 @@
 import boto3
 from organization import Organization 
 from session_manager import session_generator
-from actions.iam_role import delete_iam_role
+from actions.iam_role import delete_iam_role, iam_role_exists
 
 REGIONS = ['us-east-1', 'eu-west-1', 'eu-central-1', 'ca-central-1', 'ap-southeast-1']
 REGIONS = ['eu-west-1']
@@ -15,10 +15,13 @@ def main():
         print("action")
         # clean_kms_keys(target_session)
         # delete_stacks_with_name(target_session, "StackSet-core-deployments-servicecatalog-role")
-        approve = input("Delete IAM role? (y/n): ")
-        if approve == "y":
-            print("Deleting IAM role")
-            delete_iam_role(target_session, "mfs-devops-ci-role")    
+        if iam_role_exists(target_session, "mfs-devops-ci-role"):
+            approve = input("Delete IAM role? (y/n): ")
+            if approve == "y":
+                print("Deleting IAM role")
+                delete_iam_role(target_session, "mfs-devops-ci-role")    
+        else:
+            print("IAM role does not exist")
 
 if __name__ == '__main__':
     main()
